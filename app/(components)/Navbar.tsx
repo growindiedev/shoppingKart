2fde2c5e9cf71da47af369631d89434b425b44f1
jarryingnut <rabhishekranjan@gmail.com>
2023-01-03 19:34:12 +0530

    add sidebar drawer'

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -2,14 +2,14 @@
 const Navbar = () => {
 
 return (
-    <div className="navbar bg-base-200 fixed">
+    <div className="navbar bg-base-200">
     <div className="navbar-start">
       <div className="dropdown">
         <label tabIndex={0} className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-          <li tabIndex={0}>
+          {/* <li tabIndex={0}>
             <a className="justify-between">
               Shop
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
@@ -18,8 +18,9 @@ return (
               <li><a>Shhhh</a></li>
               <li><a>Submenu 2</a></li>
             </ul>
-          </li>
+          </li> */}
           <li><a>Contact</a></li>
+          <li><a><label htmlFor="my-drawer">Shop</label></a></li>
           <li><a>About</a></li>
         </ul>
       </div>
@@ -27,7 +28,7 @@ return (
     </div>
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
-        <li tabIndex={0}>
+        {/* <li tabIndex={0}>
           <a>
             Shop
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
@@ -36,9 +37,10 @@ return (
             <li><a>Submenu 1</a></li>
             <li><a>Submenu 2</a></li>
           </ul>
-        </li>
+        </li> */}
         <li><a>About</a></li>
-        <li><a>Contact</a></li>
+        <li><a><label htmlFor="my-drawer">Shop</label></a></li>
+        <li><a>Detour</a></li>
       </ul>
     </div>
   <div className="navbar-end">
