ee588d696ec636cfd35639c8e832be5538ca6adf
jarryingnut <rabhishekranjan@gmail.com>
2023-01-03 18:24:45 +0530

    add hero to the homepage

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
 
 return (
-    <div className="navbar bg-base-200">
+    <div className="navbar bg-base-200 fixed">
     <div className="navbar-start">
       <div className="dropdown">
         <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,18 @@ import { Inter } from '@next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div>yo mama</div>
+  return (
+    <>
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")` }}>
+    <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">SaddaGalla</h1>
+          <p className="mb-5">Check out our latest collection</p>
+          <button className="btn btn-primary">Shop</button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
 }
