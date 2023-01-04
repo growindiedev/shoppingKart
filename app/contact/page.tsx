a3da6f3f1de2fa7168e7892b2c4cdff7d10633ff
jarryingnut <rabhishekranjan@gmail.com>
2023-01-04 17:50:45 +0530

    configured routing

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>yo this is contact</div>;
+};
+
+export default page;
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is where we will render all the products</div>
+  )
+}
+
+export default page
