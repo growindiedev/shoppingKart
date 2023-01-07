7b595a7d5bc61b09ee2fde779e854c7b559ecc0d
jarryingnut <rabhishekranjan@gmail.com>
2023-01-08 01:18:42 +0530

    add loading pages and checkout component on cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,6 +2,8 @@
 import React, {useContext} from 'react'
 import { StateContext } from '../StateProvider'
 import CartItem from '../(components)/CartItem'
+import { totalItemsInCart, totalCost } from '../utils'
+import Link from 'next/link'
 
 const Page = () => {
 
@@ -22,9 +24,29 @@ const Page = () => {
   } 
 
   return (
-    <>
+    <div className="relative">
       {cartItems.map(item => <CartItem key={item.id} product={item}/>)}
-    </>
+      <div className="card card-compact glass w-1/4 bg-base-100 drop-shadow-xl fixed bottom-8 right-8">
+        <div className="card-body">
+          <span className="font-bold text-lg">{totalItemsInCart(cart)} Items</span>
+          <span className="text-info">Subtotal: ${totalCost(cart)}</span>
+          <div className="card-actions">
+            <label htmlFor="my-modal-6" className="btn btn-outline btn-secondary">Checkout</label>
+          </div>
+        </div>
+      </div>
+
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Congratulations!</h3>
+          <p className="py-4">Your order has been place. Check you email for tracking details.</p>
+          <div className="modal-action">
+            <label htmlFor="my-modal-6" className="btn">Yay!</label>
+          </div>
+        </div>
+      </div>
+    </div>
   )
   
 }
